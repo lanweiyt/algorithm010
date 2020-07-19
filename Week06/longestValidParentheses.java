421b17b9d286d0aeb91540090c85f0a36530c159
于婷 <lanw928@163.com>
2020-07-19 20:15:55 +0800

    update

---
+++ Week06/longestValidParentheses.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+public class longestValidParentheses {
+
+    //一层循环 时间复杂度O(n),空间复杂度O(n)
+    public int longestValidParentheses(String s) {
+        int maxans = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.empty()) {
+                    stack.push(i);
+                } else {
+                    maxans = Math.max(maxans, i - stack.peek());
+                }
+            }
+        }
+        return maxans;
+    }
+
+}
---
+++ Week06/minPathSum.java
@@ -0,0 +1,20 @@
+public class minPathSum {
+
+    //时间复杂度 O(mn)
+    public int minPathSum(int[][] grid) {
+        int[] dp = new int[grid.length + 1];
+        for (int i = grid.length - 1; i >= 0; i--) {
+            for (int j = grid[0].length - 1; j >= 0; j--) {
+                if(i == grid.length - 1 && j != grid[0].length - 1)
+                    dp[j] = grid[i][j] +  dp[j + 1];
+                else if(j == grid[0].length - 1 && i != grid.length - 1)
+                    dp[j] = grid[i][j] + dp[j];
+                else if(j != grid[0].length - 1 && i != grid.length - 1)
+                    dp[j] = grid[i][j] + Math.min(dp[j], dp[j + 1]);
+                else
+                    dp[j] = grid[i][j];
+            }
+        }
+        return dp[0];
+    }
+}
